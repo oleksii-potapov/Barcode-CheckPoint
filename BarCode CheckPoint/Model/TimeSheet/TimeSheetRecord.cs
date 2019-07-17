8db138bc11297db0ed16e58f20ff1621e85612e2
Oleksii Potapov <olexii.potapov@gmail.com>
2019-07-17 09:34:58 +0300

    Added TimeSheetRecord class;

---
+++ BarCode CheckPoint/Model/TimeSheet/TimeSheetRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint.Model.TimeSheet
+{
+    class TimeSheetRecord
+    {
+        public string Barcode { get; set; }
+        public string FullName { get; set; }
+        public string Post { get; set; }
+        public DateTime Date { get; set; }
+        public int DayHours { get; set; }
+        public int NightHours { get; set; }
+    }
+}
